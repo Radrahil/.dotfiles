c841bfd74ef5d4c1bf4d75789c989a508b2ef234
Rahil Prakash <54015030+Radrahil@users.noreply.github.com>
2024-03-01 12:47:06 +0530

    Initial commit

---
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,26 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>tt"] = {"<cmd> TroubleToggle <CR>"},
+  },
+}
+
+-- more keybinds!
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger",
+    }
+  }
+}
+return M
