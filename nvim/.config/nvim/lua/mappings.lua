db5f542edcd117d598e28e9b9c951170e8ec9443
Rahil Prakash <54015030+Radrahil@users.noreply.github.com>
2025-02-26 18:22:03 +0530

    Added Timerly, added menu mapping

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -105,7 +105,18 @@ end, { desc = "Close Buffer" })
 map("n", "<C-t>", function()
   require("menu").open("default")
 end, {})
+-- mouse users + nvimtree users!
+map({ "n", "v" }, "<RightMouse>", function()
+  require('menu.utils').delete_old_menus()
 
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  -- clicked buf
+  local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+  local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+
+  require("menu").open(options, { mouse = true })
+end, {})
 vim.api.nvim_create_autocmd("Filetype", {
     pattern = "norg",
     callback = function()
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -415,33 +415,11 @@ return {
 		},
 	},
 
+	"nvzone/volt",
+	{ "nvzone/timerly", cmd = "TimerlyToggle" },
 	{
 		"OXY2DEV/markview.nvim",
-		lazy = false, -- Recommended
-		-- ft = "markdown" -- If you decide to lazy-load anyway
-		config = function()
-			require("markview").setup({
-				modes = { "n", "i", "no", "c" },
-				hybrid_modes = { "i" },
-
-				-- This is nice to have
-				callbacks = {
-					on_enable = function(_, win)
-						vim.wo[win].conceallevel = 2
-						vim.wo[win].concealcursor = "nc"
-					end,
-				},
-			})
-		end,
-
-		dependencies = {
-			-- You will not need this if you installed the
-			-- parsers manually
-			-- Or if the parsers are in your $RUNTIMEPATH
-			"nvim-treesitter/nvim-treesitter",
-
-			"nvim-tree/nvim-web-devicons",
-		},
+		lazy = false,
 	},
 	-- To make a plugin not be loaded
 	-- {
